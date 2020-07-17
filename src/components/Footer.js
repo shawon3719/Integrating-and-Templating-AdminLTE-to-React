d3c1b53aa84e28c6c1dae3178610c0af3b49f70e
DIU <shawon3719@gmail.com>
2020-07-17 19:33:02 +0600

    Update Content Part

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="main-footer">
+        <strong>
+          Copyright © 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.
+        </strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.1.0-pre
+        </div>
+      </footer>
+      &gt;
+    </div>
+  );
+}
